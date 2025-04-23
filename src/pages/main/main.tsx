88b58e795ffc4cf52daa1931bef157a433dc0d92
Evans Nthiwa <evansnthiwa@gmail.com>
2025-04-23 03:59:34 +0300

    fix(main): remove extra quotes causing build failure

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,4 +1,4 @@
-""import React, { lazy, Suspense, useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
 import { useLocation, useNavigate } from 'react-router-dom';
